4411cdb884ab904df11d6dc73f2797fcc4985ff7
Javier Monmany <monmany1992@gmail.com>
2023-04-25 12:25:08 +0200

    Add router custom

---
+++ src/modules/shared/components/CustomLink.vue
@@ -0,0 +1,33 @@
+<template>
+  <a v-if="isExternalLink" :href="link.to" target="_blank">{{ link.name }}</a>
+  <!-- target="_blank" means that the link will open in a new tab -->
+  <router-link v-else :to="link.to" v-slot="{ href, isActive }"
+    ><a :href="href" :class="isActive ? 'is-active' : 'normal-link'">{{ link.name }}</a></router-link
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    isExternalLink() {
+      return this.link.to.startsWith("http");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.is-active {
+  background-color: #2196f3;
+  color: white;
+}
+.normal-link {
+  color: black;
+}
+</style>
--- src/modules/shared/components/NavBar.vue
+++ src/modules/shared/components/NavBar.vue
@@ -1,14 +1,32 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div>
-    <router-link :to="{ name: 'List' }">List</router-link>
+    <!-- <router-link :to="{ name: 'List' }">List</router-link>
     <router-link :to="{ name: 'Details', params: { id: 85 } }"
       >Details</router-link
     >
-    <router-link :to="{ name: 'About' }">About</router-link>
+    <router-link :to="{ name: 'About' }">About</router-link> -->
+    <CustomLink :to="'/'" v-for="link in links" :key="link.to" :link="link"></CustomLink>
   </div>
 </template>
-
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+    data() {
+        return {
+            links: [
+                {to: '/list', name: 'Pokemons'},
+                {to: '/about', name: 'About'},
+                {to: '/pokemon/:id', name: 'Details'},
+                {to: 'https://google.com', name: 'Google'}
+            ]
+        }
+    },
+    components: {
+        CustomLink: defineAsyncComponent(() => import('./CustomLink.vue'))
+    }
+}
+</script>
 <style scoped>
 div {
   background-color: #f1f1f1;
